fff8bd8dac058fa475f3422c0639e40c728436b7
Robert Si <robertsi0214@gmail.com>
2017-05-01 17:18:23 -0700

    added basic html and bootstrap

--- server.js
+++ server.js
@@ -2,26 +2,28 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 8080;
-var methodOverride = require('method-override');
 var _ = require('lodash');
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/app'));
 app.use(bodyParser.json());
-app.use(methodOverride('X-HTTP-Method-Override'));
 
+
+// app.use('/', function (req, res, next) {
+//   res.send('Hello World!')
+// })
 
 app.listen(port, function() {
   console.log(`Running the server on port ${port}`);
 })
 
 
-mongoose.connect('mongodb://localhost:27017/review', function (err) {
+mongoose.connect('mongodb://localhost:27017/movie', function (err) {
   if (err) {
     console.log('Not connected to the db ' + err);
   }else {
+
     console.log('Successfully connected to MongoDB');
   }
 });
